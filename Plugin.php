b78cdb8d1fc2534fb7d31cb5ef6fe2b0e7cf0b6b
miracle2017 <610010559@qq.com>
2023-01-25 01:07:46 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -57,6 +57,6 @@ class SnowstormSwitch_Plugin implements Typecho_Plugin_Interface
      */
     public static function render()
     {
-        echo '<button style="position: absolute; top: 1px; right: 1px" onclick="snowstormToggle(this)">下雪on</button><script>function snowstormToggle(e){if(snowStorm.disabled){snowStorm.resume();e.innerText="下雪OFF"}else{snowStorm.stop();e.innerText="下雪ON"}}</script>';
+        echo '<button style="position: absolute; top: 1px; right: 1px" onclick="snowstormToggle(this)">飘雪on</button><script>function snowstormToggle(e){if(snowStorm.disabled){snowStorm.resume();e.innerText="下雪OFF"}else{snowStorm.stop();e.innerText="飘雪ON"}}</script>';
     }
 }
